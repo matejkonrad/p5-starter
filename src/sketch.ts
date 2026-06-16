1f78dabb70b7c656502477dcf7515daed2c21ec4
Matej Konrad <konrad.matej@gmail.com>
2026-06-16 09:25:31 +0200

    Better shader example

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,15 +1,22 @@
 import p5 from "p5";
+import vertShader from "./shaders/gradient.vert?raw";
+import fragShader from "./shaders/gradient.frag?raw";
+//@ts-ignore
+import { resolveLygia } from "resolve-lygia";
 
 export const sketch = (p: p5) => {
+  let gradientShader: p5.Shader;
+
   p.setup = () => {
     p.createCanvas(1200, 1200, p.WEBGL);
+    gradientShader = p.createShader(resolveLygia(vertShader), resolveLygia(fragShader));
   };
 
   p.draw = () => {
-    // Draw full-screen quad
-    p.noStroke();
-    p.fill(255);
-    p.rectMode(p.CENTER);
-    p.rect(0, 0, p.width, p.height);
+    p.background(0);
+    p.stroke(255);
+
+    p.shader(gradientShader);
+    p.rect(-p.width / 2, -p.height / 2, p.width, p.height);
   };
 };
