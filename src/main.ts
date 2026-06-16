4e4210de89f85c7f02138003bda417cc95fa51b3
Matej Konrad <konrad.matej@gmail.com>
2026-06-16 08:44:05 +0200

    Update to window onload instead of main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import p5 from "p5";
 import { sketch } from "./sketch";
 import "./style.css";
 
-const main = () => {
+window.onload = () => {
   const container = document.getElementById("main");
   if (!container) {
     console.error("Canvas element not found");
@@ -11,5 +11,3 @@ const main = () => {
 
   new p5(sketch, container);
 };
-
-main();
